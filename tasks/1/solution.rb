d095bcf481e7c82e80bfccd40f84542ecd563fcd
Martin R. Hristov <martin.r.hristov@sap.com>
2017-01-15 17:47:58 +0200

    first task done

--- tasks/1/solution.rb
+++ tasks/1/solution.rb
@@ -1,49 +1,35 @@
 CELSIUS_COEFFICIENTS = {
   "C" => {
-    "initial_value" => 0,
-    "delta" => 1,
-    "formula_to_celsius" => -> (degrees) { degrees },
+    "to_celsius" => -> (degrees) { degrees },
     "celsius_to_unit" => -> (degrees) { degrees }
   },
   "F" => {
-    "initial_value" => 32,
-    "delta" => 1.8,
     "celsius_to_unit" => -> (degrees) { degrees * 1.8 + 32 },
-    "formula_to_celsius" => -> (degrees) { (degrees - 32) / 1.8 }
+    "to_celsius" => -> (degrees) { (degrees - 32) / 1.8 }
   },
   "K" => {
-    "initial_value" => 273.15,
-    "delta" => 1,
     "celsius_to_unit" => -> (celsius) { celsius + 273.15 },
-    "formula_to_celsius" => -> (degrees) { degrees - 273.15 }
+    "to_celsius" => -> (degrees) { degrees - 273.15 }
   }
 }
 
-SUBSTANCE_MELTING_POINTS = {
-  "water" => 0,
-  "ethanol" => -114,
-  "gold" => 1064,
-  "silver" => 961.8,
-  "copper" => 1085
-}
-
-SUBSTANCE_BOILING_POINTS = {
-  "water" => 100,
-  "ethanol" => 78.37,
-  "gold" => 2700,
-  "silver" => 2162,
-  "copper" => 2567
+SUBSTANCES = {
+  'water'   => { melting_point: 0,     boiling_point: 100   },
+  'ethanol' => { melting_point: -114,  boiling_point: 78.37 },
+  'gold'    => { melting_point: 1_064, boiling_point: 2_700 },
+  'silver'  => { melting_point: 961.8, boiling_point: 2_162 },
+  'copper'  => { melting_point: 1_085, boiling_point: 2_567 }
 }
 
 def convert_between_temperature_units(degrees, convert_from_unit, convert_to_unit)
-  celsius = CELSIUS_COEFFICIENTS[convert_from_unit]["formula_to_celsius"].call(degrees)
+  celsius = CELSIUS_COEFFICIENTS[convert_from_unit]["to_celsius"].call(degrees)
   CELSIUS_COEFFICIENTS[convert_to_unit]["celsius_to_unit"].call(celsius)
 end
 
 def melting_point_of_substance(substance, unit)
-  convert_between_temperature_units(SUBSTANCE_MELTING_POINTS[substance], "C", unit)
+  convert_between_temperature_units(SUBSTANCES[substance][:melting_point], "C", unit)
 end
 
 def boiling_point_of_substance(substance, unit)
-  convert_between_temperature_units(SUBSTANCE_BOILING_POINTS[substance], "C", unit)
+  convert_between_temperature_units(SUBSTANCES[substance][:boiling_point], "C", unit)
 end
