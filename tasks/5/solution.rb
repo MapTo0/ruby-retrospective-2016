170a923bb93125bd9cb28f38a3bc58df052f14cc
Martin R. Hristov <martin.r.hristov@sap.com>
2017-01-15 19:50:03 +0200

    final solution

--- tasks/5/solution.rb
+++ tasks/5/solution.rb
@@ -1,197 +1,157 @@
-module DataModelUtilities
-  def save
-    if self.class.data_store.include? @settings
-      update_store
-    else
-      create_instance_id(self.class)
-      self.class.data_store.create(@settings)
-    end
-    self
+class HashStore
+  attr_reader :storage
+
+  def initialize
+    @storage = {}
+    @id_counter = 0
   end
 
-  def delete
-    if @data_store.include? @settings
-      @data_store.delete(@settings)
-    else
-      raise DataModel::DeleteUnsavedRecordError, 'intance is not saved'
-    end
+  def next_id
+    @id_counter += 1
   end
 
-  def ==(entity)
-    if self.class == entity.class
-      same_attributes = (self.settings.to_a - entity.settings.to_a).empty?
-      if same_attributes && (@data_store.include? entity.settings)
-        self.id == entity.id
-      else
-        self.object_id == entity.object_id
-      end
+  def create(record)
+    @storage[record[:id]] = record
+  end
+
+  def find(query)
+    @storage.values.select do |record|
+      query.all? { |key, value| record[key] == value }
     end
   end
 
-  private
-  def update_store
-    updates = {}
-    @attributes.each_key do |key|
-      updates[key] = self.send(key)
-      @settings[key] = updates[key]
-    end
-    @data_store.update(@settings[:id], updates)
+  def delete(query)
+    find(query).each { |record| @storage.delete(record[:id]) }
   end
 
-  def create_instance_id class_name
-    @id = class_name.class_variable_get(last_id)
-    class_name.last_id += 1
-    @settings[:id] = @id
+  def update(id, record)
+    return unless @storage.key? id
+
+    @storage[id] = record
   end
 end
 
-module DataModelErrors
-  class DeleteUnsavedRecordError < ::StandardError
+class ArrayStore
+  attr_reader :storage
+
+  def initialize
+    @storage = []
+    @id_counter = 0
   end
 
+  def next_id
+    @id_counter += 1
+  end
+
+  def create(record)
+    @storage << record
+  end
+
+  def find(query)
+    @storage.select { |record| match_record? query, record }
+  end
+
+  def delete(query)
+    @storage.reject! { |record| match_record? query, record }
+  end
+
+  def update(id, record)
+    index = @storage.find_index { |record| record[:id] == id }
+    return unless index
+
+    @storage[index] = record
+  end
+
+  private
+
+  def match_record?(query, record)
+    query.all? { |key, value| record[key] == value }
+  end
+end
+
+module DataModelClassMethods
+  def attributes(*attributes)
+    return @attributes if attributes.empty?
+
+    @attributes = attributes + [:id]
+
+    @attributes.each do |attribute|
+      define_singleton_method "find_by_#{attribute}" do |value|
+        where(attribute => value)
+      end
+
+      define_method(attribute)       { @attributes[attribute] }
+      define_method("#{attribute}=") { |value| @attributes[attribute] = value }
+    end
+  end
+
+  def data_store(store = nil)
+    return @data_store unless store
+
+    @data_store = store
+  end
+
+  def where(query)
+    query.keys.reject { |key| @attributes.include? key }.each do |key|
+      raise DataModel::UnknownAttributeError.new(key)
+    end
+
+    map_to_model_instances data_store.find(query)
+  end
+
+  def all
+    where({})
+  end
+
+  private
+
+  def map_to_model_instances(records)
+    records.map { |record| new(record) }
+  end
+end
+
+class DataModel
   class UnknownAttributeError < ArgumentError
     def initialize(attribute_name)
       super "Unknown attribute #{attribute_name}"
     end
   end
-end
 
-module DataModelMethods
-  def attributes(*attribute_store)
-    if attribute_store.empty?
-      @attributes.keys
+  class DeleteUnsavedRecordError < StandardError
+  end
+
+  extend DataModelClassMethods
+
+  def initialize(attributes = {})
+    @attributes = attributes.select { |key, _| self.class.attributes.include? key }
+  end
+
+  def save
+    if id
+      data_store.update(id, @attributes)
     else
-      @attributes = {}
-      save_attributes(attribute_store)
+      self.id = data_store.next_id
+      data_store.create(@attributes)
     end
-  end
 
-  def data_store(store = nil)
-    @data_store = store if store
-    @data_store
-  end
-
-  def where(entities = {})
-    result = []
-    @data_store.each do |setting|
-      if (entities.keys - @attributes.keys).empty?
-        if (entities.to_a - setting.to_a).empty?
-          result << self.new(setting) && result
-        end
-      else
-        raise DataModel::UnknownAttributeError.new((entities.keys - @attributes.keys)[0])
-      end
-    end
-  end
-
-  def take_attributes
-    @attributes
-  end
-
-  private
-  def save_attributes(attribute_store)
-    attribute_store.each_index do |index|
-      @attributes[attribute_store[index]] = true
-       method_name = ("find_by_" + attribute_store[index].to_s).to_sym
-       define_singleton_method(method_name) do |attribute|
-         where({ attribute_store[index] => attribute })
-       end
-    end
-  end
-end
-
-class DataModel
-  include DataModelUtilities
-  include DataModelErrors
-  @data_store = nil
-  @last_id = 1
-  attr_accessor :id, :settings
-  def initialize(settings = {})
-    @settings = settings
-    @attributes = self.class.take_attributes
-    @attributes.each_key do |key|
-      self.class.send(:attr_accessor, key)
-      instance_variable_set("@#{key}", settings[key])
-    end
     self
   end
 
-  extend DataModelMethods
-end
+  def delete
+    raise DeleteUnsavedRecordError.new unless id
 
-class BaseStore
-  include Enumerable
-  attr_accessor :store, :id
+    data_store.delete(id: id)
+  end
 
-  def find(hash)
-    result = []
-    each do |setting|
-      result << setting if (hash.to_a - setting.to_a).empty?
-    end
-    result
+  def ==(other)
+    return id == other.id if id && other.id
+
+    equal? other
+  end
+
+  private
+
+  def data_store
+    self.class.data_store
   end
 end
-
-class ArrayStore < BaseStore
-  def initialize
-    @store = []
-  end
-
-  def each
-    @store.each do |member|
-      yield(member)
-    end
-  end
-
-  def include?(hash)
-    @store.include? hash
-  end
-
-  def create(hash)
-    @store.push hash
-  end
-
-  def update(id, updates)
-    model = @store.select { |model| model[:id] == id }.first
-    updates.each_key do |key|
-      model[key] = updates[key]
-    end
-  end
-end
-
-class HashStore < BaseStore
-  def initialize
-    @store = {}
-  end
-
-  def each
-    @store.each do |_, value|
-      yield(value)
-    end
-  end
-
-  def include?(hash)
-    @store[:id] == hash
-  end
-
-  def create(hash)
-    @store[hash[:id]] = hash
-  end
-
-  def update(id, updates)
-    model = @store[id.to_sym]
-    updates.each_key do |key|
-      model[key] = updates[key]
-    end
-  end
-end
-
-class User < DataModel
-  attributes :name, :email
-  data_store ArrayStore.new
-end
-
-record = User.new(name: 'Ivan', email: 'Ivanov')
-record.save
-
-expect(User.find_by_name('Ivan').map(&:id)).to eq [record.id]
